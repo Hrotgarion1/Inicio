d1881b697bd19050afac140f1706b001c918b661
Jose P. Núñez <thyrion1976@gmail.com>
2023-12-08 09:30:49 +0100

    traducidas las vistas de Auth

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\ComponenteController;
+use App\Http\Controllers\Seguridad\RolesController;
+use App\Http\Controllers\Seguridad\UsersController;
+use Illuminate\Support\Facades\Session;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,14 +29,17 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', function () { return Inertia::render('Dashboard'); })->name('dashboard');
+
+    Route::get('productos/create', [ProductoController::class, 'create'])
+    ->middleware(['check.permission'])
+    ->name('productos.create');
+
+    Route::resource('productos', ProductoController::class);
+    Route::resource('componentes', ComponenteController::class);
+    Route::get('reports', [ComponenteController::class, 'reports'])->name('reports');
 });
 
 //Ruta protegida por el middleware CheckPermission que otorga permiso solo al rol admin
@@ -40,3 +48,9 @@ Route::middleware(['App\Http\Middleware\CheckPermission:admin'])->group(function
     Route::resource('roles', RolesController::class);
     Route::get('permisos', [Seguridad\PermisosController::class, 'index'])->name('permisos.index');
 });
+
+//Ruta para los idiomas
+Route::get('/language/{language}', function($language){
+    Session::put('locale', $language);
+    return redirect()->back();
+})->name('language');
