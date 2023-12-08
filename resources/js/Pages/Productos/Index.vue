5ec72a91af8c5e43d5f255e646fa6d082a8914e1
Jose P. Núñez <thyrion1976@gmail.com>
2023-12-08 14:21:37 +0100

    eliminado el paquete vite-generate-pwa-assets

--- resources/js/Pages/Productos/Index.vue
+++ resources/js/Pages/Productos/Index.vue
@@ -83,7 +83,7 @@ const ok = (msj) => {
                 <div class="mt-3 mb-4 flex justify-self-end">
                 <Link :href="route('productos.create')"
                 :class="'inline-flex items-center px-4 py-2 bg-green-600 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 hover:bg-green-700 dark:hover:bg-white focus:bg-green-700 dark:focus:bg-white active:bg-green-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150'">
-                <i class="fa-solid fa-plus-circle"></i> {{$t('Add')}}
+                <i class="fa-solid fa-plus-circle"><span class="ml-2">{{$t('Add')}}</span></i> 
                 </Link>
                 </div>
 
