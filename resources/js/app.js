855303f02f4c08d96b885abeb836c49d31a5f366
Jose P. Núñez <thyrion1976@gmail.com>
2023-12-09 16:06:36 +0100

    cambiado el nombre en app js

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { i18nVue } from 'laravel-vue-i18n';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'TcxStocks';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
