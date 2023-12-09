3a4d02a4cc733bcfa5c25f875f8feba2e75806d6
Jose P. Núñez <thyrion1976@gmail.com>
2023-12-09 15:49:57 +0100

    añadidos favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'TcxStocks') }}</title>
+
+        <!-- iconos -->
+        <link rel="icon" href="/favicon.ico" sizes="16x16" type="image/x-icon">
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png">
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png">
+        <link rel="icon" href="/favicon-96x96.png" sizes="96x96" type="image/png">
+        <link rel="icon" href="/favicon-256x256.png" sizes="256x256" type="image/png">
+        <link rel="icon" href="/ms-icon-70x70.png" sizes="70x70" type="image/png">
+        <link rel="icon" href="/ms-icon-144x144.png" sizes="144x144" type="image/png">
+        <link rel="icon" href="/ms-icon-150x150.png" sizes="150x150" type="image/png">
+        <link rel="icon" href="/ms-icon-310x310.png" sizes="310x310" type="image/png">
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
